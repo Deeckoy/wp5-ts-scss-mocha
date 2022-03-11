a1b357c5aa197085c623f7f987cc6b9755788126
Edward Haussauer <edhaussauer@gmail.com>
2022-03-11 18:08:24 +0200

    wp5-ts-scss-mocha

---
+++ webpack.config.js
@@ -0,0 +1,90 @@
+const webpack = require('webpack');
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const config = {
+	entry: './src/index.ts',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].[contenthash].js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							importLoaders: 1,
+						},
+					},
+					'postcss-loader',
+				],
+			},
+			{
+				test: /\.ts(x)?$/,
+				loader: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.scss$/,
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.svg$/,
+				use: 'file-loader',
+			},
+			{
+				test: /\.png$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							mimetype: 'image/png',
+						},
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		new CopyPlugin({
+			patterns: [{ from: 'src/index.html' }],
+		}),
+		new HtmlWebpackPlugin({
+			// templateContent: ({ htmlWebpackPlugin }) => '<!DOCTYPE html><html><head><meta charset=\"utf-8\"><title>' + htmlWebpackPlugin.options.title + '</title></head><body><div id=\"app\"></div></body></html>',
+			title: 'Webpack Boilerplate',
+			template: path.resolve(__dirname, './src/template.html'),
+			filename: 'index.html',
+		}),
+		new BundleAnalyzerPlugin({
+			analyzerMode: 'static',
+			openAnalyzer: false,
+		}),
+		new MiniCssExtractPlugin(),
+		new CleanWebpackPlugin(),
+	],
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	optimization: {
+		runtimeChunk: 'single',
+		splitChunks: {
+			cacheGroups: {
+				vendor: {
+					test: /[\\/]node_modules[\\/]/,
+					name: 'vendors',
+					chunks: 'all',
+				},
+			},
+		},
+	},
+};
+
+module.exports = config;
